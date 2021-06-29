a596e4e09499b81806df6a231d46090898f88ac0
UesleiFerraz <uesleivaleriod@gmail.com>
2021-06-29 17:20:59 -0300

    added styles on auth page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
-import '../styles/auth.scss'
+import "../styles/auth.scss";
 
 export function Home() {
   return (
@@ -18,19 +18,14 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button>
-            <img src={googleIconImg} alt="LOgo do Google" />
+          <button className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
             Cria sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
           <form>
-            <input 
-              type="text"
-              placeholder="Digite o código da sala"
-            />
-            <button type="submit">
-              Entrar na sala
-            </button>
+            <input type="text" placeholder="Digite o código da sala" />
+            <button type="submit">Entrar na sala</button>
           </form>
         </div>
       </main>
